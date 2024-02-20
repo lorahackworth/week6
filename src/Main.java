a7dd8d0f09b723750871c149452772fce5d5bf38
lorahackworth <lora.g.hackworth@gmail.com>
2024-02-20 14:16:49 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            System.out.println("Please enter a number: ");
+            int num1 = input.nextInt();
+            System.out.println(num1);
+            System.out.println("Please enter a second number: ");
+            int num2 = input.nextInt();
+            System.out.println(num2);
+
+            add(num1, num2);
+            divide(num1, num2);
+        }catch(InputMismatchException e){
+            System.out.println("Incorrect data type.");
+            System.out.println(e);
+        }
+
     }
+
+
+    static void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+    static void divide(double a, double b){
+        System.out.println(a/b);
+    }
+
+
 }
